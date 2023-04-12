8ff9d8bf8d4ed4420f3a97a1e0b37bcdbbef68bc
synth.wav <hcpreik@ncsu.edu>
2023-04-11 21:57:17 -0400

    Formatting fixes

--- FinchArchiveExporter.cs
+++ FinchArchiveExporter.cs
@@ -2,13 +2,20 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.CodeDom;
+using System.Text.RegularExpressions;
 
 namespace FinchArchiveExporter {
     class ParseArchive {
+
+        public const int SEPARATOR_LENGTH = 50;
+        public const int CREATION_TIME_OFFSET = 21;
+        public const int ENTRY_BODY_OFFSET = 24;
+        public const int SKIP_OFFSET = 43;
+        public const String BULLET_TYPE_2 = "\"bullet_type\":2";
         static void Main(string[] args) {
             String fileName;
             List<Entry> entries = new List<Entry>();
-            entries.Sort((x, y) => DateTime.Compare(x.entryTime, y.entryTime));
             if (args.Length == 0 || args.Length > 1) {
                 Console.WriteLine("Usage: FinchArchiveParser inputFile.json");
             }
@@ -33,43 +40,101 @@ namespace FinchArchiveExporter {
                 System.Environment.Exit(1);
             }
             while(nextBulletPos > 0) {
-                String entryBody = lineTrimmed.Substring(24, (lineTrimmed.IndexOf("\",") - 24));
+                String entryBody = lineTrimmed.Substring(ENTRY_BODY_OFFSET, (lineTrimmed.IndexOf("\",") - ENTRY_BODY_OFFSET));
+
+                //Remove these characters in the body, they're used for the sentiment/subject analysis stuff
+                // but we don't want to see them in our export
                 entryBody = entryBody.Replace("#","");
-                entryBody = entryBody.Replace("_", "");
-                //Console.WriteLine("Text body is: {0}", entryBody);
-                String entryTimeString = lineTrimmed.Substring(lineTrimmed.IndexOf("creation_time") + 21,  20);
+                entryBody = entryBody.Replace("_", " ");
+
+                //Fixing a strange output formatting error where "\n\n" in plaintext appears in the body text
+                entryBody = entryBody.Replace("\\n\\n", "");
+                int charSinceSpace = 0;
+                int charCount = 0;
+                
+                foreach (char c in entryBody)
+                {
+                    //Padding the entry body text for readability
+                    if (c == ' ') {
+                        if (charSinceSpace > 50)
+                        {
+                            entryBody = entryBody.Insert(charCount, "\n");
+                            entryBody = entryBody.Remove(charCount + 1, 1);
+                            charSinceSpace = 0;
+                        }
+                    }
+                    //Conditional removing of exclamation points if they aren't followed by a space
+                    //As these are also included for emotion identification in your entries.
+                    //I don't know if this rule applies in other languages but I always have a space
+                    //after an '!' so that's how it's going to work
+                    if (c == '!')
+                    {
+                        //Console.WriteLine("{0}, {1}", entryBody.Length - 1, charCount);
+                        if (charCount != entryBody.Length && charCount != entryBody.Length - 1 && entryBody[charCount + 1] != ' ')
+                        {
+                            entryBody = entryBody.Remove(charCount, 1);
+                            charCount--;
+                        }
+                    }
+                    charSinceSpace++;
+                    charCount++;
+                }
+
+                //Grab the time created
+                String entryTimeString = lineTrimmed.Substring(lineTrimmed.IndexOf("creation_time")
+                    + CREATION_TIME_OFFSET, CREATION_TIME_OFFSET - 1);
+                
+                //Remove extra quote if date is shorter
                 entryTimeString = entryTimeString.Replace("\"", "");
-                //Console.WriteLine("Time string is {0}", entryTimeString);
+                
+                //Parse the DateTime from the substring
                 DateTime entryTime = DateTime.Parse(entryTimeString);
+
+                //Console.WriteLine("Text body is: {0}", entryBody);
+                //Console.WriteLine("Time string is {0}", entryTimeString);
                 //Console.WriteLine("DateTime is {0}", entryTime.ToString());
+
+                //Create a new instance of Entry with the time and body text and add it to our list of Entries
                 Entry currentEntry = new Entry(entryTime, entryBody);
                 entries.Add(currentEntry);
-                line = lineTrimmed.Substring(line.IndexOf("creation_time") + 43);
-                nextBulletPos = line.IndexOf("\"bullet_type\":2");
+
+                //Move forward to the next reflection entry
+                line = lineTrimmed.Substring(line.IndexOf("creation_time") + SKIP_OFFSET);
+                nextBulletPos = line.IndexOf(BULLET_TYPE_2);
                 if(nextBulletPos > 0)
                     lineTrimmed = line.Substring(nextBulletPos);
             }
 
+            //Sort the entries by their entryTime
             entries.Sort();
+            //Create output file
             using (FileStream fs = File.Create("FinchArchiveExport" + DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss") + ".txt"))
             {
                 using (var fw = new StreamWriter(fs))
                 {
+                    //Write header
                     fw.WriteLine("Finch Journal Entry Export " + DateTime.Now.ToString("D"));
-                    fw.WriteLine("-------------------------------------------------------------------\n");
-                    DateTime currentDate = entries[0].entryTime.Date.AddDays(-1);
-                    Console.WriteLine(currentDate.ToString());
-                    fw.WriteLine(currentDate.ToString("D") + "\n");
+                    fw.WriteLine(string.Concat(Enumerable.Repeat("-", SEPARATOR_LENGTH)) + "\n");
+
+                    //Write the earliest date in the entry list at the top
+                    DateTime currentDate = entries[0].entryTime.Date;
+                    fw.WriteLine(currentDate.ToString("D"));
+                    fw.WriteLine(string.Concat(Enumerable.Repeat("-", SEPARATOR_LENGTH)));
+
+                    //Write each entry
                     foreach (Entry entry in entries)
                     {
+                        //Update and print the current date if the current entry is on a new day
                         if (entry.entryTime.Date > currentDate)
                         {
-                            currentDate = entry.entryTime.Date.AddDays(-1);
-                            fw.WriteLine("\n" + currentDate.ToString("D") + "\n");
-                            Console.WriteLine("\n" + currentDate.ToString("D") + "\n");
+                            currentDate = entry.entryTime.Date;
+                            fw.WriteLine("\n" + currentDate.AddDays(-1).ToString("D"));
+                            //Console.WriteLine("\n" + currentDate.ToString("D") + "\n");
+                            fw.WriteLine(string.Concat(Enumerable.Repeat("-", SEPARATOR_LENGTH)));
                         }
-                        fw.WriteLine("{0}: {1}", entry.entryTime.ToShortTimeString(), entry.entryBody);
-                        Console.WriteLine("{0}: {1}", entry.entryTime.ToShortTimeString(), entry.entryBody);
+                        //Write the current entry
+                        fw.WriteLine("{0}: {1}\n", entry.entryTime.ToShortTimeString(), entry.entryBody);
+                        //Console.WriteLine("{0}: {1}", entry.entryTime.ToShortTimeString(), entry.entryBody);
                     }
                 }
             }
@@ -78,7 +143,10 @@ namespace FinchArchiveExporter {
     }
 
     class Entry : IComparable<Entry> {
+        //The creation time of the entry
         public DateTime entryTime;
+
+        //The body text of the entry
         public String entryBody;
 
         public Entry(DateTime myEntryTime, String myEntryBody) {
@@ -86,6 +154,7 @@ namespace FinchArchiveExporter {
             this.entryBody = myEntryBody;
         }
 
+        //Compares entries based on their creation time
         int IComparable<Entry>.CompareTo(Entry other)
         {
             return DateTime.Compare(this.entryTime, other.entryTime);
